8e4967897cf6963691bf7b52343bee9e3c48a3d6
Arjun01 <erarjun0@gmail.com>
2020-08-12 15:09:13 +0530

    created post-create component

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+
+
+})
+
+export class PostCreateComponent{
+
+}
