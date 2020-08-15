bab4da3f5fd6f068a559407dd88f0cef19d3ebb6
Arjun01 <erarjun0@gmail.com>
2020-08-15 16:13:46 +0530

    adding POST backend point

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers","Origin","X-Requested-With", "Content-Type", "Accept");
-  res.setHeader("Access-Control-Allow-Methods", "GET", "POST", "PATCH", "DELETE", "OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers","Origin,X-Requested-With, Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
   next();
 });
 
-app.use('/api/posts',(req,res,next) => {
+
+
+app.post("/api/posts",(req, res, next) =>{
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({message: 'Post added Successfully'});
+});
+
+app.get('/api/posts',(req,res,next) => {
   const posts = [
     { id: "asldjfalsfsa00001",
       title:'First server side post',
